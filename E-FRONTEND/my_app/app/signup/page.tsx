16287e2a45613208a5dfe767ac8ad5246cc2d218
RabbiXX <@rabiabdul12345@gmail.com>
2026-09-24 19:42:55 +0100

    Feat: Complete responsive signup page route and initial Next.js project configuration

---
+++ E-FRONTEND/my_app/app/signup/page.tsx
@@ -0,0 +1,47 @@
+export default function SignupPage() {
+  return (
+    <div className="w-full min-h-screen py-3 px-4 bg-[url('/images/background.jpg')] bg-cover bg-center">
+      <div className="max-w-5xl p-4 mx-auto grid grid-cols-1 md:grid-cols-2 items-center rounded-3xl bg-[#5c2c05]/90">
+        <div className="w-48 sm:w-64 md:w-80 aspect-square rounded-full  mx-auto flex justify-center items-center outline-2 outline-amber-900 bg-gray-100 border-2  border-gray-900">
+          <div className="w-2/3 aspect-square"><img className="w-full h-full object-contain" src="/images/logo.jpg" alt="Logo" /></div>
+        </div>
+
+        <div className="mt-5 flex justify-center items-center text-white border-2 border-gray-700 bg-[#f3e2d5]/70 rounded-2xl ">
+          <div className="w-75 mx-auto flex gap-4 flex-col py-5">
+            <h2 className="text-4xl text-black text-center">Create Account</h2>
+            <div className="w-full">
+              <p className="text-lg text-[#281404]">Name</p>
+              <input type="text" className="stuff w-full p-2 rounded-lg bg-[#603410]/80 text-white placeholder-white placeholder:text-sm focus:outline-hidden focus:ring-2 focus:ring-amber-500" placeholder="Enter your name" />
+            </div>
+            <div className="w-full">
+              <p className="text-lg text-[#281404]">Email</p>
+              <input type="text" className="w-full p-2 rounded-lg bg-[#603410]/80 text-white placeholder-white placeholder:text-sm  focus:outline-hidden focus:ring-2 focus:ring-amber-500" placeholder="Enter your email" alt="email" />
+            </div>
+            <div className="w-full">
+              <p className="text-lg text-[#281404]">Password</p>
+              <input type="password" className="w-full p-2 rounded-lg bg-[#603410]/80 text-white placeholder-white placeholder:text-sm focus:outline-hidden focus:ring-2 focus:ring-amber-500" placeholder="Enter your password" alt="password" />
+            </div>g
+            <div className="mt-3 ">
+              <div className="w-full mb-3">
+                <button className="w-full p-2 rounded-lg bg-[#603410] text-white hover:bg-[#311905] transition duration-300">Create Account</button>
+              </div>
+              <div className="w-full">
+                <button className="w-full bg-white flex items-center justify-center p-2 rounded-lg text-black hover:bg-[#695547] transition duration-300">
+                  <div className="mx-2 bg-white p-0.5 rounded-full"><img className="h-5 rounded-full" src="/images/google-logo.png" alt="" /></div>Sign up with Google
+                </button>
+              </div>
+              <div className="w-full mt-3 flex justify-center">
+                <a href="#"><button className=" bg-[#957762] text-[#1f1004] flex flex-col items-center justify-center px-4 py-3 rounded-lg hover:bg-[#502d14] transition duration-300 hover:text-[#ae9c8e]">Already have an account?<span className="text-[#904407] ml-1.5"> Login here</span>
+                </button></a>
+              </div>
+            </div>
+
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
